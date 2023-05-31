af566fd1cbcaa5436b69d9bff1133037180b57dd
Saddiqq <9adi8.tm@gmail.com>
2023-05-31 12:24:38 +0400

    Setup Configuration

--- demo/src/main/java/com/example/demo/CronExpressionApplication.java
+++ demo/src/main/java/com/example/demo/CronExpressionApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CronExpressionApplication {
 
 	public static void main(String[] args) {
--- demo/src/main/java/com/example/demo/schedule.java
+++ demo/src/main/java/com/example/demo/schedule.java
@@ -1,6 +1,11 @@
 package com.example.demo;
 
-
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+@Component
 public class schedule {
-
+@Scheduled(cron = "0 30 17 1 * *")
+    public void schedule(){
+    System.out.println("Test1");
+}
 }
